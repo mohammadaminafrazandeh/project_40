fd11bfd87593c8a9b50d04decebd440174e5968f
Amin <124354256+xzierox@users.noreply.github.com>
2023-09-09 19:17:27 +0330

    Initial commit

---
+++ lib/sign_up_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  const SignUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff1252F7),
+        elevation: 5,
+      ),
+      body: Container(
+        width: double.infinity,
+        color: Color(0xffD5EAFB),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/logo.png',
+              width: 115,
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xff1252F7)),
+                  ),
+                  label: Text(
+                    'Email',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  suffixIcon: Container(
+                    margin: EdgeInsets.only(right: 20.0),
+                    child: Icon(Icons.remove_red_eye),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xff1252F7)),
+                  ),
+                  label: Text(
+                    'Password',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30), color: Colors.white),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  suffixIcon: Container(
+                    margin: EdgeInsets.only(right: 20.0),
+                    child: Icon(Icons.remove_red_eye),
+                  ),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xfff1f1fb)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide(color: Color(0xff1252F7)),
+                  ),
+                  label: Text(
+                    'Confirm Password',
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              height: 45,
+              margin: EdgeInsets.fromLTRB(20, 10, 10, 20),
+              decoration:
+                  BoxDecoration(borderRadius: BorderRadius.circular(30)),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('دکـــمه کــــودتا کـــ'),
+                style: TextButton.styleFrom(
+                    backgroundColor: Color(0xff1252F7),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30))),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
